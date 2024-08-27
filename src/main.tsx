16f8a1de835afbbc3ed77260e866dfca3b581678
franpax95 <fcojnavarrowork@gmail.com>
2024-08-27 19:43:33 +0200

    feat(main): add language provider to root

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client';
 import App from './app/App.tsx';
 import './index.scss';
 import { StorageProvider } from '@contexts/StorageContext';
+import { LanguageProvider } from '@app/contexts/LanguageContext/index.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <StorageProvider>
-      <App />
+      <LanguageProvider>
+        <App />
+      </LanguageProvider>
     </StorageProvider>
   </StrictMode>
 );
