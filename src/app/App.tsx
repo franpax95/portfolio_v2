0994a79a5205cdcc79d42152447e074190628288
franpax95 <fcojnavarrowork@gmail.com>
2024-08-27 19:15:07 +0200

    feat(app): sidebar positioning

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,14 @@
+import { Sidebar } from './components/Sidebar';
+import './App.scss';
+
 function App() {
-  return <div className="app">Vite + React Skeleton</div>;
+  return (
+    <div className="app">
+      <Sidebar></Sidebar>
+
+      <div className="app__main">{/** About me */} About me </div>
+    </div>
+  );
 }
 
 export default App;
