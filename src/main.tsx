d1d0c96cc357b7ad00f1e9d89324f7df8bda39db
franpax95 <fcojnavarrowork@gmail.com>
2024-08-27 19:37:37 +0200

    feat(main): add storage provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app/App.tsx';
 import './index.scss';
+import { StorageProvider } from '@contexts/StorageContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <StorageProvider>
+      <App />
+    </StorageProvider>
   </StrictMode>
 );
