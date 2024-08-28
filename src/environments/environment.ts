924bbb3b1f92f8b4fe974627582446d747acd690
franpax95 <fcojnavarrowork@gmail.com>
2024-08-28 20:40:13 +0200

    feat(environment): add github and linkedin links

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,9 @@ import { Lang } from '@states/lang';
 export const environment = {
   defaults: {
     lang: Lang.Eng,
+  },
+  links: {
+    linkedin: 'https://www.linkedin.com/in/fco-navarro/',
+    github: 'https://github.com/franpax95',
   }
 };
