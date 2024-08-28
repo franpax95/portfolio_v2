983d4e54f89e1496bf843b796dbc2072c40dd91e
franpax95 <fcojnavarrowork@gmail.com>
2024-08-28 15:31:48 +0200

    feat(lang): add initial esp, eng & deu

---
+++ src/lang/deu.tsx
@@ -0,0 +1,14 @@
+import { Sections } from '@states/sections';
+
+export default {
+  [Sections.Sidebar]: {
+    title: 'Francisco Navarro',
+    subtitle: 'Frontend Developer',
+    description: 'I build pixel-perfect, engaging, and accessible digital experiences.'
+  },
+  [Sections.Menu]: {
+    about: 'About',
+    experience: 'Experience',
+    projects: 'Projects'
+  }
+};
---
+++ src/lang/esp.tsx
@@ -0,0 +1,14 @@
+import { Sections } from '@states/sections';
+
+export default {
+  [Sections.Sidebar]: {
+    title: 'Francisco Navarro',
+    subtitle: 'Desarrollador Frontend',
+    description: 'Construyo páginas'
+  },
+  [Sections.Menu]: {
+    about: 'Sobre mí',
+    experience: 'Experiencia',
+    projects: 'Proyectos'
+  }
+};
