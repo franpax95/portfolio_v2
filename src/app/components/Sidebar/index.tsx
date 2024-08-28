ff79f1ab4fcdea252895091f02ddfd130dc24200
franpax95 <fcojnavarrowork@gmail.com>
2024-08-28 19:16:27 +0200

    feat(sidebar): useLanguage usage

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -1,5 +1,7 @@
+import { useLanguage } from '@app/hooks/useLanguage';
 import { Menu } from '../Menu';
 import './styles.scss';
+import { Sections } from '@states/sections';
 
 // export interface SidebarProps {
 //   className: string;
@@ -7,13 +9,15 @@ import './styles.scss';
 // }
 
 export const Sidebar = (/*{ className, children }: SidebarProps*/) => {
+  const { txt } = useLanguage(Sections.Sidebar);
+
   return (
     <div className="sidebar">
-      <h1>Francisco Navarro</h1>
+      <h1>{txt.title}</h1>
 
-      <h2>Frontend Developer</h2>
+      <h2>{txt.subtitle}</h2>
 
-      <p>I build pixel-perfect, engaging, and accessible digital experiences.</p>
+      <p>{txt.description}</p>
 
       <Menu />
     </div>
