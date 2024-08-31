725afb1f4654b7b553ebd49423df9b9722c4a7bc
franpax95 <fcojnavarrowork@gmail.com>
2024-08-31 10:18:07 +0200

    feat(projectsTable): projects table initial commit

---
+++ src/app/components/ProjectsTable/index.tsx
@@ -0,0 +1,43 @@
+import { useLanguage } from '@hooks/useLanguage';
+import './styles.scss';
+import { Sections } from '@states/sections';
+import { IProject } from '@interfaces/IProject';
+import { TechBadge } from '../TechBadge';
+
+export interface ProjectsTableProps {
+  projects: Array<IProject>;
+}
+
+export const ProjectsTable = ({ projects }: ProjectsTableProps) => {
+  const { txt } = useLanguage(Sections.ProjectsTable);
+
+  return (
+    <div className="projects-table">
+      <div className="projects-table__header">
+        <div className="projects-table__col">{txt.year}</div>
+        <div className="projects-table__col">{txt.project}</div>
+        <div className="projects-table__col">{txt.madeAt}</div>
+        <div className="projects-table__col">{txt.technologies}</div>
+      </div>
+
+      {projects.map((project, key) => (
+        <div className="projects-table__row" key={key}>
+          <div className="projects-table__col" title={`${project.year}`}>
+            {project.year}
+          </div>
+          <div className="projects-table__col" title={project.name}>
+            {project.name}
+          </div>
+          <div className="projects-table__col" title={project.job}>
+            {project.job}
+          </div>
+          <div className="projects-table__col projects-table__col--stack">
+            {project.stack.map((tech) => (
+              <TechBadge tech={tech} />
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
