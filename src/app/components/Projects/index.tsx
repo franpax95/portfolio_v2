ab098bfa406b4d75c89d12c5f72bf7c9dc337432
franpax95 <fcojnavarrowork@gmail.com>
2024-08-31 10:18:30 +0200

    feat(projects): section

---
+++ src/app/components/Projects/index.tsx
@@ -0,0 +1,91 @@
+// import { useLanguage } from '@hooks/useLanguage';
+// import { Sections } from '@states/sections';
+import './styles.scss';
+import { IProject } from '@interfaces/IProject';
+import { ProjectsTable } from '../ProjectsTable';
+
+// const esail1_techs = ['sass', 'typescript', 'angular', 'react', 'laravel', 'java', 'ionic', 'git'];
+// const kubide_techs = ['sass', 'typescript', 'angular', 'next', 'ionic', 'docker', 'git', 'node'];
+// const esail2_techs = ['sass', 'typescript', 'angular', 'git'];
+
+const esail = 'ESAIL IT SOLUTIONS';
+const kubide = 'Kubide';
+
+const projects: Array<IProject> = [
+  {
+    year: 2024,
+    name: 'CeballosERP',
+    job: esail,
+    stack: ['html', 'sass', 'typescript', 'angular']
+  },
+  {
+    year: 2023,
+    name: 'Codify',
+    job: kubide,
+    stack: ['html', 'sass', 'typescript', 'angular', 'ionic', 'mongodb', 'android', 'ios']
+  },
+  {
+    year: 2023,
+    name: 'Onyze',
+    job: kubide,
+    stack: ['html', 'sass', 'typescript', 'react', 'next']
+  },
+  {
+    year: 2023,
+    name: 'Excalibur',
+    job: kubide,
+    stack: ['html', 'scss', 'typescript', 'angular', 'mongodb', 'android', 'ios']
+  },
+  {
+    year: 2023,
+    name: 'AtiendeQ',
+    job: esail,
+    stack: ['html', 'sass', 'typescript', 'angular', 'ionic']
+  },
+  {
+    year: 2023,
+    name: 'Prolongo Ronda Presencias',
+    job: esail,
+    stack: ['html', 'sass', 'typescript', 'angular']
+  },
+  {
+    year: 2023,
+    name: 'Prolongo Red Agentes',
+    job: esail,
+    stack: ['html', 'sass', 'typescript', 'angular']
+  },
+  {
+    year: 2022,
+    name: 'espacioT',
+    job: esail,
+    stack: ['html', 'css', 'react']
+  },
+  {
+    year: 2022,
+    name: 'VPeio Gestor Imágenes',
+    job: esail,
+    stack: ['html', 'css', 'angular', 'java', 'spring']
+  },
+  {
+    year: 2021,
+    name: 'Tramicar',
+    job: esail,
+    stack: ['html', 'css', 'angular', 'java', 'spring']
+  },
+  {
+    year: 2021,
+    name: 'VPeio Gestión Interna',
+    job: esail,
+    stack: ['html', 'css', 'angular', 'java', 'spring']
+  }
+];
+
+export const Projects = () => {
+  // const { txt } = useLanguage(Sections.Experience);
+
+  return (
+    <div className="projects">
+      <ProjectsTable projects={projects} />
+    </div>
+  );
+};
