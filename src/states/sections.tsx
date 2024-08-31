e9eccc0b957f35509227a3d229d5b75f6d2cfcd2
franpax95 <fcojnavarrowork@gmail.com>
2024-08-31 10:08:42 +0200

    feat(states): add experience, projects & projectstable sections

--- src/states/sections.tsx
+++ src/states/sections.tsx
@@ -1,5 +1,8 @@
 export enum Sections {
   Sidebar = 'sidebar',
   Menu = 'menu',
-  About = 'about'
+  About = 'about',
+  Experience = 'experience',
+  Projects = 'projects',
+  ProjectsTable = 'projects_table'
 }
