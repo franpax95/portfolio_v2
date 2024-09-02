2ade2a20467beed7c51f972a64216ba62ffa09db
franpax95 <fcojnavarrowork@gmail.com>
2024-09-02 16:18:11 +0200

    fix(mainAnchor): correct svg react properties

--- src/app/components/MainAnchor/index.tsx
+++ src/app/components/MainAnchor/index.tsx
@@ -34,8 +34,8 @@ export const MainAnchor = ({ href, stopPropagation, target, children }: MainAnch
           width="24"
           height="24"
           xmlns="http://www.w3.org/2000/svg"
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
         >
           <path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z" />
         </svg>
@@ -45,8 +45,8 @@ export const MainAnchor = ({ href, stopPropagation, target, children }: MainAnch
           width="24"
           height="24"
           xmlns="http://www.w3.org/2000/svg"
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
         >
           <path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z" />
         </svg>
