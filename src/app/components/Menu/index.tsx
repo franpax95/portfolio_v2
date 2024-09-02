63a47416c9176dd8de78c9f61da0b0e809e354ef
franpax95 <fcojnavarrowork@gmail.com>
2024-09-02 14:00:09 +0200

    feat(menu): use intersection observer usage with links

--- src/app/components/Menu/index.tsx
+++ src/app/components/Menu/index.tsx
@@ -1,16 +1,22 @@
 import './styles.scss';
+import { useIntersectionObserver } from '@hooks/useIntersectionObserver';
 
-// export interface MenuProps {
-//   className: string;
-//   children: React.ReactNode;
-// }
+const sections = ['about', 'experience', 'projects', 'recognitions'];
 
 export const Menu = () => {
+  const active = useIntersectionObserver(sections);
+
   return (
     <div className="menu">
-      <a className="menu__link">About</a>
-      <a className="menu__link">Experience</a>
-      <a className="menu__link">Projects</a>
+      <a className={`menu__link ${active === 'about' && 'menu__link--active'}`}>About</a>
+
+      <a className={`menu__link ${active === 'experience' && 'menu__link--active'}`}>Experience</a>
+
+      <a className={`menu__link ${active === 'projects' && 'menu__link--active'}`}>Projects</a>
+
+      <a className={`menu__link ${active === 'recognitions' && 'menu__link--active'}`}>
+        Recognitions
+      </a>
     </div>
   );
 };
